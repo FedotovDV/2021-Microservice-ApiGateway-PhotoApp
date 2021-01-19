55c7fc0856f049393af805d4059ccbce7b6b7fdb
Dmitriy <fed3375546@yandex.ru>
2021-01-19 16:39:43 +0300

    Initial commit

---
+++ src/test/java/ru/fed3375546/photoapp/api/gateawy/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.fed3375546.photoapp.api.gateawy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
